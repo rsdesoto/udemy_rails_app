320950941c335c844e5dd1449c3682b33e48e9d6
Ry DeSoto <ry.e.desoto@gmail.com>
2020-06-15 11:49:16 -0500

    Error validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    # require the top level key article, then permit the subkeys title and desc
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+  end
 end
