b4d7bfb76148058f456526ecc75e97a1c81ca01d
Ry DeSoto <ry.e.desoto@gmail.com>
2020-06-27 21:12:54 -0500

    Password starting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200627192354) do
+ActiveRecord::Schema.define(version: 20200628020743) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20200627192354) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
